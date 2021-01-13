3b8393147862180fd08773812e4dbbcccdffeb27
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-01-13 17:56:49 +0530

    Icon and Player

--- Space_Invader/main.py
+++ Space_Invader/main.py
@@ -4,9 +4,27 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
+running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
