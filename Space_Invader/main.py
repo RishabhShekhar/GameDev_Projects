163f104ce540319026bc8706ea7bc077c94f1291
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-01-14 02:08:37 +0530

    Enemy Movememnt

--- Space_Invader/main.py
+++ Space_Invader/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -11,10 +12,20 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('space-invaders.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+
+enemyImg = pygame.image.load('alien.png')
+enemyX = random.randint(0,736)
+enemyY = random.randint(50,150)
+enemyX_change = 1
+enemyY_change = 40
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 running = True
@@ -26,5 +37,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        if event.type == pygame.KEYDOWN:
+            print("Keystroke is pressed")
+            if event.key == pygame.K_LEFT:
+                print("Left")
+                playerX_change -= 0.5
+            if event.key == pygame.K_RIGHT:
+                print("Right")
+                playerX_change += 0.5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Released")
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 1
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -1
+        enemyY += enemyY_change
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
