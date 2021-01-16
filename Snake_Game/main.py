567c69abe0eb03e5b663e68b41cdcb65978c2a0b
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-01-16 12:10:09 +0530

    Final Commit

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 
 pygame.init()
 
@@ -6,21 +8,30 @@ screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("SNAKE GAME")
 
-running = True
+score = 0
 
-snake_pos = [[300, 300], [340, 300], [380, 300], [420, 300]]
+snake_pos = [[300, 300], [320, 300], [340, 300], [360, 300]]
 
-step = 30
+step = 20
 up = (0, -step)
 left = (-step, 0)
 right = (step, 0)
 down = (0, step)
 direction = left
 
+apple_pos = [260, 300]
+
 timer = 0
 
+font = pygame.font.SysFont('Arial', 30)
+
+game_over = False
+
+running = True
+
 while running:
-    pygame.time.Clock().tick(30)
+
+    pygame.time.Clock().tick(50)
     screen.fill((20, 100, 20))
 
     for event in pygame.event.get():
@@ -41,8 +52,27 @@ while running:
         snake_pos = [[snake_pos[0][0] + direction[0], snake_pos[0][1] + direction[1]]] + snake_pos[:-1]
         timer = 0
 
+    if snake_pos[0] == apple_pos:
+        x = (random.randint(20, 780)//20) * 20
+        y = (random.randint(20, 580) // 20) * 20
+        apple_pos = [x, y]
+        snake_pos.append(snake_pos[-1])
+        score += 1
+
+    for i in range(1 ,len(snake_pos)):
+        if snake_pos[0] == snake_pos[i]:
+            game_over = True
+
+    if game_over:
+       print(score)
+
     for x, y in snake_pos:
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 20)
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 10)
+
+    pygame.draw.circle(screen, (0, 0, 255), apple_pos, 10)
+
+    text = font.render(("SCORE: " + str(score)), True, (255,255,255))
+    screen.blit(text, (0, 0))
 
     pygame.display.update()
 
