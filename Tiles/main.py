e76f2fd8004f752787b4e2f26851b75c1143bac6
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-01-22 14:39:54 +0530

    Arcade Mode

--- Tiles/main.py
+++ Tiles/main.py
@@ -19,10 +19,8 @@ def arcade_mode():
     clicked = False
 
     game_font = pygame.font.Font("freesansbold.ttf", 32)
-    font = pygame.font.Font("freesansbold.ttf", 32)
-    xy = [100, 100]
 
-    sCoord = (10,10)
+    xy = [100, 100]
 
     def generate_box(x, y):
         return pygame.Rect(x, y, 100, 100)
@@ -34,18 +32,22 @@ def arcade_mode():
         global score
         if xy[0] < mx < xy[0] + 100 and xy[1] < my < xy[1] + 100:
             score += 1
-            print(score)
             return True
         return False
 
-    clicked = False
-    TimeOutRun = True
+    def draw_lifes(l):
+        lifes = lx
+        for i in range(lifes):
+            pygame.draw.circle(screen, (100,100,0), (760 - 30*i, 20), 15)
+
+
+    ArcadeRun = True
     start = pygame.time.get_ticks()
-    while TimeOutRun:
+    while ArcadeRun:
         screen.fill((200, 200, 200))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                TimeOutRun = False
+                ArcadeRun = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # LEFT CLICK
                     clicked = True
@@ -58,7 +60,9 @@ def arcade_mode():
         mx, my = pygame.mouse.get_pos()
 
         current_time = pygame.time.get_ticks()
-        if current_time - start > 1000:
+        if current_time - start > 1000 and not (clicked):
+            life -= 1
+            print(life)
             start = pygame.time.get_ticks()
             xy = [random.randint(0, 700), random.randint(0, 500)]
 
@@ -68,9 +72,17 @@ def arcade_mode():
                 pygame.draw.rect(screen, (255, 0, 0), box)
                 start = pygame.time.get_ticks()
 
-        game_time = pygame.time.get_ticks()
+            elif (current_time - start < 1000) and not isClicked(xy, mx, my):
+                clicked = False
+                life -= 1
+                pygame.draw.rect(screen, (0, 0, 255), box)
+                xy = [random.randint(0, 700), random.randint(0, 500)]
+                start = pygame.time.get_ticks()
+                print(life)
 
-        if game_time - start_time >= 60000:
+        draw_lifes(life)
+
+        if life <= 0:
             screen.fill((200, 200, 200))
             msg = pygame.font.Font('freesansbold.ttf', 64)
             screen.blit(msg.render("GAME OVER!!", True, (0, 0, 0)), (180, 200))
@@ -166,9 +178,22 @@ while MainRun:
             if event.button == 1:
                 clicked = False
 
-    # box = generate_box(xy[0], xy[1])
-    # pygame.draw.rect(screen, (255, 0, 0), box)
     mx, my = pygame.mouse.get_pos()
+
+    Welcome_Message = font_64.render("TILES", True, (0, 0, 200))
+    screen.blit(Welcome_Message, (sw // 2 - 110, 20))
+
+    Select_Mode = font_32.render("SELECT MODE:", True, (0, 0, 200))
+    screen.blit(Select_Mode, (10, sh - 400))
+
+    time_out = font_32.render("TIME-OUT", True, (0, 0, 200))
+    screen.blit(time_out, (290, sh - 360))
+
+    arcade = font_32.render("ARCADE", True, (0, 0, 200))
+    screen.blit(arcade, (290, sh - 320))
+
+    Start = font_64.render("START", True, (0, 0, 200))
+    screen.blit(Start, (sw // 2 - 80, sh - 100))
 
     if clicked and state == 0:
         if 285 < mx < 420 and sh - 360 < my < sh - 323:
@@ -186,36 +211,8 @@ while MainRun:
         if mode == 1:
             MainRun = False
             time_out_mode(pygame.time.get_ticks())
-        elif mode == 1:
+        elif mode == 2:
             MainRun = False
             arcade_mode()
 
-    Welcome_Message = font_64.render("TILES", True, (0, 0, 200))
-    screen.blit(Welcome_Message, (sw // 2 - 110, 20))
-
-    Select_Mode = font_32.render("SELECT MODE:", True, (0, 0, 200))
-    screen.blit(Select_Mode, (10, sh - 400))
-
-    time_out = font_32.render("TIME-OUT", True, (0, 0, 200))
-    screen.blit(time_out, (290, sh - 360))
-
-    arcade = font_32.render("ARCADE", True, (0, 0, 200))
-    screen.blit(arcade, (290, sh - 320))
-
-    Start = font_64.render("START", True, (0, 0, 200))
-    screen.blit(Start, (sw // 2 - 80, sh - 100))
-
-    # current_time = pygame.time.get_ticks()
-    # if current_time - start > 1000:
-    #     start = pygame.time.get_ticks()
-    #     xy = [random.randint(0, 700), random.randint(0, 500)]
-    #
-    # if clicked:
-    #     if (current_time - start < 1000) and isClicked(xy, mx, my):
-    #         xy = [random.randint(0, 700), random.randint(0, 500)]
-    #         pygame.draw.rect(screen, (255, 0, 0), box)
-    #         start = pygame.time.get_ticks()
-    #
-    # print_score(score)
-    # clock.tick(60)
     pygame.display.update()
