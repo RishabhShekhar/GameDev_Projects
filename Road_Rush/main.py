e1030de182750ba806c74d1170ad0a8715162ba2
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-01-24 17:50:18 +0530

    Road Rush Images sound

--- Road_Rush/main.py
+++ Road_Rush/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -9,6 +10,13 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 light_road = pygame.image.load("imgs/light_road.png")
 dark_road = pygame.image.load("imgs/dark_road.png")
+car = pygame.image.load("imgs/car.png")
+truck = pygame.image.load("imgs/truck.png")
+rock = pygame.image.load("imgs/rock.png")
+health = pygame.image.load("imgs/health.png")
+
+health_sound = pygame.mixer.Sound("sounds/health.wav")
+rock_sound = pygame.mixer.Sound("sounds/rock.wav")
 
 texture_position = 0
 
@@ -22,6 +30,37 @@ texture_position_acceleration = 4
 texture_position_threshold = 300
 half_texture_position_threshold = texture_position_threshold//2
 
+car_x = 260
+car_y = 360
+stone_x = random.randint(250, 350)
+stone_y = 240
+health_x = random.randint(250, 350)
+health_y = 240
+
+state = 0
+
+score = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+sCoord = (10, 10)
+def score_print(scr):
+    screen.blit(font.render("Score: "+ str(scr), True, (0, 0, 0)), sCoord)
+
+def isCollided(Cx, Cy, Sx, Sy):
+    if Cx+20<Sx+15<Cx+110 and Cy+20<Sy+11<Cy+110:
+        return True
+    return False
+
+def draw_lives(l):
+    pygame.draw.rect(screen, (200, 0, 0), (600-30*4, 10, 30*5, 15))
+    for i in range(l):
+        pygame.draw.rect(screen, (0,200,0), (600-30*i, 10, 30, 15))
+
+life = 5
+game = 1
+start_time = pygame.time.get_ticks()
+life_time = pygame.time.get_ticks()
+health_piece = 0
+
 while True:
     pygame.time.Clock().tick(30)
     screen.fill((0, 0, 200))
@@ -30,25 +69,29 @@ while True:
             pygame.quit()
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        car_x += 5
+        if car_x >= 450:
+            car_x -= 5
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_LEFT]:
+        car_x -= 5
+        if car_x <= 50:
+            car_x += 5
+
+
+    if life>0:
         road_pos += road_acceleration
         if road_pos >= texture_position_threshold:
             road_pos = 0
-
-    elif keys[pygame.K_DOWN]:
-        road_pos -= road_acceleration
-        if road_pos <= 0:
-            road_pos = texture_position_threshold
-
 
     texture_position = road_pos
     dz = 0
     z = 0
 
-    for i in range(half_screen_height-1,-1,-1):
+    for i in range(half_screen_height - 1, -1, -1):
         if texture_position < half_texture_position_threshold:
-            screen.blit(light_road, (0, i+half_screen_height), (0, i, screen_width, 1))
+            screen.blit(light_road, (0, i + half_screen_height), (0, i, screen_width, 1))
         else:
             screen.blit(dark_road, (0, i + half_screen_height), (0, i, screen_width, 1))
 
@@ -59,4 +102,26 @@ while True:
         if texture_position >= texture_position_threshold:
             texture_position = 0
 
+    game_time = pygame.time.get_ticks()
+    if game_time - start_time > 1000 and state == 0:
+        state = 1
+        stone_x = random.randint(250, 350)
+        stone_y = 240
+        chng = 0
+
+    if state == 1 and life>0:
+        stone_y += 5
+        if stone_x
+        screen.blit(rock, (stone_x, stone_y))
+        if stone_y >= 480:
+            state = 0
+            start_time = pygame.time.get_ticks()
+
+
+
+    screen.blit(car,(car_x, car_y))
+    screen.blit(truck, (270, 210))
+
+    score_print(score)
+    draw_lives(life)
     pygame.display.update()
