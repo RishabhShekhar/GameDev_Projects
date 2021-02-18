f499e54c2971f7e74aa2576bafc521cd3c1b29ac
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-02-18 06:45:45 +0530

    Update client.py

--- Multiplayer_Game/client.py
+++ Multiplayer_Game/client.py
@@ -1,5 +1,7 @@
 import pygame
 
+pygame.init()
+
 screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -7,15 +9,55 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
-def redrawWindow():
+
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.val = 3
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.val
+        if keys[pygame.K_RIGHT]:
+            self.x += self.val
+        if keys[pygame.K_UP]:
+            self.y -= self.val
+        if keys[pygame.K_DOWN]:
+            self.y += self.val
+
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def redrawWindow(screen, player):
     screen.fill((255, 255, 255))
+    player.draw(screen)
     pygame.display.update()
 
+
 def main():
+
+    clock = pygame.time.Clock()
     run = True
+    p = Player(50, 50, 100, 100, (0, 255, 0))
 
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
+                # pygame.quit()
+
+        p.move()
+        redrawWindow(screen, p)
+
+main()
