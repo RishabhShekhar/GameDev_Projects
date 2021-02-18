b85a18c86e7f34da6f75174e0d83da986fe6a39c
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-02-18 07:07:22 +0530

    Update server.py

--- Multiplayer_Game/server.py
+++ Multiplayer_Game/server.py
@@ -1,8 +1,8 @@
 import socket
-import _thread
+from _thread import *
 import sys
 
-server = ""
+server = "192.168.29.48"  # Local IP Address
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,4 +15,28 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server started")
 
-def
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
